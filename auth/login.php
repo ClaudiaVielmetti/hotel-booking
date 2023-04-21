fb4eb622673a09242cba1017b43c2f46e7ea1de8
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-04-21 15:17:08 +0200

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -1,6 +1,50 @@
 <?php require "../includes/header.php"; ?>
 <?php require "../config/config.php"; ?> 
 
+<?php 
+
+//VALIDATE FORM SUBMISSION
+if(isset($_POST['submit'])) {
+ if(empty($_POST['username']) OR empty($_POST['email']) OR empty($_POST['password'])) {
+      echo "<script>alert('One or more inputs are empty')</script>";
+    } else {
+
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+
+
+      //validate email with query
+
+      $login = $conn->query("SELECT * FROM users WHERE email='$email'");
+      $login->execute();
+
+      $fetch = $login->fetch(PDO::FETCH_ASSOC);
+
+
+      //get row count
+
+      if($login->rowCount() > 0) {
+
+        if(password_verify($password, $fetch['mypassword'])) {
+          echo "<script>alert('LOGGED IN')</script>";
+        } else {
+
+          echo "<script>alert('EMAIL OR PASSWORD IS INCORRECT')</script>";
+        }
+
+      } else {
+
+        echo "<script>alert('EMAIL OR PASSWORD IS INCORRECT')</script>";
+      }
+
+
+    }
+}
+
+
+
+?>
+
     <div class="hero-wrap js-fullheight" style="background-image: url('images/image_2.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
@@ -18,27 +62,27 @@
     	<div class="container">
 	    	<div class="row justify-content-middle" style="margin-left: 397px;">
 	    		<div class="col-md-6 mt-5">
-						<form action="#" class="appointment-form" style="margin-top: -568px;">
+						<form action="login.php" method="POST" class="appointment-form" style="margin-top: -568px;">
 							<h3 class="mb-3">Login</h3>
 							<div class="row">
 								<div class="col-md-12">
 									<div class="form-group">
-			    					    <input type="text" class="form-control" placeholder="Email">
+			    					    <input type="text" name="email" class="form-control" placeholder="Email">
 			    				    </div>
 								</div>
                                
-                                <div class="col-md-12">
+                  <div class="col-md-12">
 									<div class="form-group">
-			    					    <input type="password" class="form-control" placeholder="Password">
+			    					    <input type="password" name="password" class="form-control" placeholder="Password">
 			    				    </div>
 								</div>
 								
 							
 							
 								<div class="col-md-12">
-                                    <div class="form-group">
-                                        <input type="submit" value="Login" class="btn btn-primary py-3 px-4">
-                                    </div>
+                     <div class="form-group">
+                         <input type="submit" name="submit" value="Login" class="btn btn-primary py-3 px-4">
+                     </div>
 								</div>
 							</div>
 	    			</form>
