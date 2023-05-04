30ca6470760a08f5799d5bb8678424d1f1afb3ca
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-05-04 12:57:12 +0200

    Navigation buttons

--- includes/header.php
+++ includes/header.php
@@ -59,15 +59,15 @@ define("APPURL", "http://localhost/hotel-booking/hotel-booking");
 			</button>
 			<div class="collapse navbar-collapse" id="ftco-nav">
 				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+					<li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
 					<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
 					<li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
 					<li class="nav-item"><a href="rooms.html" class="nav-link">Apartment Room</a></li>
 					<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
 
 					<?php if(!isset($_SESSION['username'])) : ?>
-						<li class="nav-item"><a href="login.html" class="nav-link">Login</a></li>
-						<li class="nav-item"><a href="register.html" class="nav-link">Register</a></li>
+						<li class="nav-item"><a href="<?php echo APPURL; ?>/auth/login.php " class="nav-link">Login</a></li>
+						<li class="nav-item"><a href="<?php echo APPURL; ?>/auth/register.php" class="nav-link">Register</a></li>
 					<?php else : ?>	
 
 
