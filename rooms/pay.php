b86c95a5bfe59c2304e08fc035a40330aafbd1dc
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-05-09 15:41:12 +0200

    Update pay.php

--- rooms/pay.php
+++ rooms/pay.php
@@ -6,30 +6,29 @@
     <script src="https://www.paypal.com/sdk/js?client-id=AZvKrg1e58TRN4byOOAGbDKgReTlduLAu3Rr5VjAQpYbvWLP7ro5kJ_BInixNAd30BirEUcuuANWO1DN&currency=USD"></script>
     <!-- Set up a container element for the button -->
     <div id="paypal-button-container"></div>
-    <script>
-        paypal.Buttons({
-            // Sets up the transaction when a payment button is clicked
-            createOrder: (data, actions) => {
-                return actions.order.create({
-                    purchase_units: [{
-                        amount: {
-                            value: '<?php echo $payment; ?>' // Can also reference a variable or function
-                        }
-                    }]
-                });
-            },
-            // Finalize the transaction after payer approval
-            onApprove: (data, actions) => {
-                return actions.order.capture().then(function(orderData) {
+        <script>
+            paypal.Buttons({
+                // Sets up the transaction when a payment button is clicked
+                createOrder: (data, actions) => {
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: '<?php echo $payment; ?>' // Can also reference a variable or function
+                            }
+                        }]
+                    });
+                },
+                // Finalize the transaction after payer approval
+                onApprove: (data, actions) => {
+                    return actions.order.capture().then(function(orderData) {
 
-                    window.location.href = 'index.php';
-                });
-            }
-        }).render('#paypal-button-container');
-    </script>
+                        window.location.href = 'index.php';
+                    });
+                }
+            }).render('#paypal-button-container');
+        </script>
 
-</div>
-</div>
+    </div>
 </div>
 
 
