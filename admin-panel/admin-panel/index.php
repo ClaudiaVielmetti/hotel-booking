eee2317df4d0412e4f36e4a1aebe2a8ea964ccb0
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-05-10 16:22:42 +0200

    number of hotels

--- admin-panel/admin-panel/index.php
+++ admin-panel/admin-panel/index.php
@@ -1,14 +1,25 @@
 <?php require 'layouts/header.php'; ?>
-<!-- <?php
+<?php require 'admins/config/config-admin.php'; ?>
 
+
+<?php 
+
+if(!isset($_SESSION['admin_name'])){
+  echo "<script>window.location.href='".ADMINURL."/admins/login-admins.php';</script>";
   
-  // if(isset($_SESSION['admin_name'])){
-  //   echo "<script>window.location.href='" .ADMINURL. "/admins/login-admins.php' </script>";
-  // }
-?> -->
+}
+
+//Hotel Count
+
+  $hotels = $conn->query("SELECT COUNT(*) AS count_hotels FROM `hotels`");
+  $hotels->execute();
+
+  $alllHotels = $hotels->fetch(PDO::FETCH_OBJ);
+
+?>
 
 
-<div class="container-fluid">
+
 
   <div class="row">
     <div class="col-md-4">
@@ -16,7 +27,7 @@
         <div class="card-body">
           <h5 class="card-title">Hotels</h5>
           <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-          <p class="card-text">number of hotels: 8</p>
+          <p class="card-text">Number of Hotels: <?php echo $alllHotels->count_hotels; ?></p>
 
         </div>
       </div>
