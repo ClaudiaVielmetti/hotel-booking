3636bd000bd4f58aaeb35d8d083b97bba8bedd8c
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-05-10 10:32:10 +0200

    Update pay.php

--- rooms/pay.php
+++ rooms/pay.php
@@ -1,7 +1,7 @@
 <?php require "../includes/header.php"; ?>
 <?php require "../config/config.php"; ?>
 
-<div class="hero-wrap js-fullheight" style="background-image: url('images/image_2.jpg');" data-stellar-background-ratio="0.5">
+<div class="hero-wrap js-fullheight" style="background-image: url('<?php echo APPURL; ?>/images/image_2.jpg');" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
