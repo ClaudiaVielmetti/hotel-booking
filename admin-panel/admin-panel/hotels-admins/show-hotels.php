81249950d23425245b07ab2755b4bebcc87953dc
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-05-11 11:04:17 +0200

    looping dynamically through tables

--- admin-panel/admin-panel/hotels-admins/show-hotels.php
+++ admin-panel/admin-panel/hotels-admins/show-hotels.php
@@ -4,72 +4,56 @@
 <?php
 
 
-  if (!isset($_SESSION['admin_name'])) {
-    echo "<script>window.location.href='" . ADMINURL . "/admins/login-admins.php' </script>";
-  }
+if (!isset($_SESSION['admin_name'])) {
+  echo "<script>window.location.href='" . ADMINURL . "/admins/login-admins.php' </script>";
+}
 
-  $admins = $conn->query("SELECT * FROM admins");
-  $admins->execute();
+$hotels = $conn->query("SELECT * FROM hotels");
+$hotels->execute();
 
-  $allAdmins = $admins->fetchAll(PDO::FETCH_OBJ);
+$allHotels = $hotels->fetchAll(PDO::FETCH_OBJ);
 
 ?>
 
 
-          <div class="row">
-        <div class="col">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title mb-4 d-inline">Hotels</h5>
-             <a  href="create-hotels.html" class="btn btn-primary mb-4 text-center float-right">Create Hotels</a>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">name</th>
-                    <th scope="col">location</th>
-                    <th scope="col">status value</th>
-                    <th scope="col">change status</th>
-                    <th scope="col">update</th>
-                    <th scope="col">delete</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Sheraton</td>
-                    <td>Cairo</td>
-                    <td>1</td>
+<div class="row">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title mb-4 d-inline">Hotels</h5>
+        <a href="create-hotels.html" class="btn btn-primary mb-4 text-center float-right">Create Hotels</a>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">name</th>
+              <th scope="col">location</th>
+              <th scope="col">status value</th>
+              <th scope="col">change status</th>
+              <th scope="col">update</th>
+              <th scope="col">delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($allHotels as $hotel) : ?>
+              <tr>
+                <th scope="row"><?php echo $hotel->id; ?></th>
+                <td><?php echo $hotel->name; ?></td>
+                <td><?php echo $hotel->location; ?></td>
+                <td>><?php echo $hotel->status; ?></td>
 
-                    <td><a  href="status.html" class="btn btn-warning text-white text-center ">status</a></td>
-                    <td><a  href="update-category.html" class="btn btn-warning text-white text-center ">Update </a></td>
-                    <td><a href="delete-category.html" class="btn btn-danger  text-center ">Delete </a></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Sheraton</td>
-                    <td>Cairo</td>
-                    <td>1</td>
+                <td><a href="status-hotels.php?id=<?php echo $hotel->id; ?>" class="btn btn-warning text-white text-center ">status</a></td>
+                <td><a href="update-category.php?id=<?php echo $hotel->id; ?>" class="btn btn-warning text-white text-center ">Update </a></td>
+                <td><a href="delete-category.php?id=<?php echo $hotel->id; ?>" class="btn btn-danger  text-center ">Delete </a></td>
+              </tr>
+            <?php endforeach; ?>
 
-                    <td><a  href="status.html" class="btn btn-warning text-white text-center ">status</a></td>
-                    <td><a  href="update-category.html" class="btn btn-warning text-white text-center ">Update </a></td>
-                    <td><a href="delete-category.html" class="btn btn-danger  text-center ">Delete</a></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Sheraton</td>
-                    <td>Cairo</td>
-                    <td>1</td>
-                    <td><a  href="status.html" class="btn btn-warning text-white text-center ">status</a></td>
-                    <td><a  href="update-category.html" class="btn btn-warning text-white text-center ">Update</a></td>
-                    <td><a href="delete-category.html" class="btn btn-danger  text-center ">Delete</a></td>
-                  </tr>
-                </tbody>
-              </table> 
-            </div>
-          </div>
-        </div>
+          </tbody>
+        </table>
       </div>
+    </div>
+  </div>
+</div>
 
 
 <?php require '../layouts/footer.php'; ?>
